adf207375f723475eeb8819feda0ca64476b36bf
DavidCai <davidcai1993@yahoo.com>
2017-02-02 13:21:55 +0800

    remove match and use map_or

--- src/decode.rs
+++ src/decode.rs
@@ -7,84 +7,72 @@ pub fn decode(b: &Bytes) -> Result<Data, &str> {
 fn decode_with_last_pos<'a>(b: &Bytes, start: usize) -> (Result<Data, &'a str>, usize) {
     match b[start] {
         STRING_PREFIX => {
-            match parse(b, start + 1) {
-                Ok((s, i)) => (Ok(Data::String(String::from_utf8(s).unwrap())), i),
-                Err(e) => (Err(e), 0),
-            }
+            parse(b, start + 1).map_or((Err("Invalid bytes"), 0), |(s, i)| {
+                (Ok(Data::String(String::from_utf8(s).unwrap())), i)
+            })
         }
         ERROR_PREFIX => {
-            match parse(b, start + 1) {
-                Ok((e, i)) => (Ok(Data::Error(String::from_utf8(e).unwrap())), i),
-                Err(e) => (Err(e), 0),
-            }
+            parse(b, start + 1).map_or((Err("Invalid bytes"), 0),
+                                       |(e, i)| (Ok(Data::Error(String::from_utf8(e).unwrap())), i))
         }
         INT_PREFIX => {
-            match parse(b, start + 1) {
-                Ok((i, pos)) => {
-                    (Ok(Data::Integer(String::from_utf8(i)
-                         .unwrap()
-                         .parse::<i64>()
-                         .unwrap())),
-                     pos)
-                }
-                Err(e) => (Err(e), 0),
-            }
+            parse(b, start + 1).map_or((Err("Invalid bytes"), 0), |(i, pos)| {
+                (Ok(Data::Integer(String::from_utf8(i)
+                     .unwrap()
+                     .parse::<i64>()
+                     .unwrap())),
+                 pos)
+            })
         }
         BULK_PREFIX => {
-            match parse(b, start + 1) {
-                Ok((ref bl, bulk_start_index)) => {
-                    let bulk_len: usize = String::from_utf8(bl.to_vec())
-                        .unwrap()
-                        .parse::<usize>()
-                        .unwrap();
-                    let bulk_end_index: usize = bulk_start_index + bulk_len;
-                    let bulk: Bytes = b[bulk_start_index..bulk_end_index].to_vec();
+            parse(b, start + 1).map_or((Err("Invalid bytes"), 0), |(bl, bulk_start_index)| {
+                let bulk_len: usize = String::from_utf8(bl.to_vec())
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap();
+                let bulk_end_index: usize = bulk_start_index + bulk_len;
+                let bulk: Bytes = b[bulk_start_index..bulk_end_index].to_vec();
 
-                    (Ok(Data::BulkString(String::from_utf8(bulk).unwrap())),
-                     bulk_start_index + bulk_len + 1)
-                }
-                Err(e) => (Err(e), 0),
-            }
+                (Ok(Data::BulkString(String::from_utf8(bulk).unwrap())),
+                 bulk_start_index + bulk_len + 1)
+            })
         }
         ARRAY_PREFIX => {
-            match parse(b, start + 1) {
-                Ok((ref a, mut pos)) => {
-                    let arr_len: usize = String::from_utf8(a.to_vec())
-                        .unwrap()
-                        .parse::<usize>()
-                        .unwrap();
-                    let mut result: Vec<Data> = Vec::new();
+            parse(b, start + 1).map_or((Err("Invalid bytes"), 0), |(a, mut pos)| {
+                let arr_len: usize = String::from_utf8(a.to_vec())
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap();
+                let mut result: Vec<Data> = Vec::new();
 
-                    for _ in 0..arr_len {
-                        println!("{}", 1);
-                        let (res, i) = decode_with_last_pos(b, pos);
-                        match res {
-                            Ok(data) => {
-                                result.push(data);
-                                pos = i;
-                            }
-                            Err(e) => return (Err(e), 0),
+                for _ in 0..arr_len {
+                    println!("{}", 1);
+                    let (res, i) = decode_with_last_pos(b, pos);
+                    match res {
+                        Ok(data) => {
+                            result.push(data);
+                            pos = i;
                         }
+                        Err(e) => return (Err(e), 0),
                     }
-
-                    (Ok(Data::Array(result)), pos)
                 }
-                Err(e) => (Err(e), 0),
-            }
+
+                (Ok(Data::Array(result)), pos)
+            })
         }
 
         _ => (Err("Missing prefix"), 0),
     }
 }
 
-fn parse<'a>(b: &Bytes, start: usize) -> Result<(Bytes, usize), &'a str> {
+fn parse(b: &Bytes, start: usize) -> Option<(Bytes, usize)> {
     for i in start..b.len() - 1 {
         if b[i] == CRLF[0] && b[i + 1] == CRLF[1] {
-            return Ok((b[start..i].to_vec(), i + 2));
+            return Some((b[start..i].to_vec(), i + 2));
         }
     }
 
-    Err("Invalid bytes")
+    None
 }
 
 #[cfg(test)]
